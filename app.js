32b3d0ce3df0d110dbcad1677a229c803bf9ba9f
unknown <test@test.com>
2018-12-24 12:25:02 +0200

    =Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname+'/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        welcomeMessage: 'Welcome',
+        pageTitle: 'Welcome Page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page'
+    });  
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
